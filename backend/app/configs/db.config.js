6f957c1bad88ebc9667dd464633e3784f3f21a99
GUSTAVO MULLER KRELLING <gustavo.krelling@alunos.sc.senac.br>
2023-05-16 11:38:12 -0300

    Projeto Lanchonete

---
+++ backend/app/configs/db.config.js
@@ -0,0 +1,8 @@
+//Arquivo de configuraçoes do Banco de daods
+module.exports = {
+    HOST:"localhost",
+    USER: "root",
+    PASSWORD:"",
+    DB:"xdev",
+    PORT:"3306"
+}
---
+++ backend/app/models/db.js
@@ -0,0 +1,21 @@
+//Modulo do MYSQL
+const mysql = require("mysql");
+const dbConfig = require("../configs/db.config.js");
+
+//Cria uma conexao com o BD
+
+const connection = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB,
+    port: dbConfig.PORT,
+});
+
+//Nova conexao com o Mysql
+connection.connect(error=>{
+   if(error) throw error;
+   console.log("Banco de dados conectado!!!!!!!");
+});
+
+module.exports = connection;
